c5905f7219f73253434d3ed8e956ffa8389a6c44
Adrian Jozwik <adrianj105@gmail.com>
2019-12-31 12:15:56 +0000

    add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express();
 const port = process.env.port || 8000;
 
 
+const mockUserData=[
+    {name: "Mark"},
+    {name: "Jill"}
+]
+
+app.get("/users", (req, res) => {
+    res.json({
+        success: true,
+        message: "successfuly got users. Nice!",
+        users: mockUserData
+    })
+})
+
 
 app.listen(port, () => {
     console.log("Server is running");
